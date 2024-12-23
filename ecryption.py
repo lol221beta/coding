963ac9a7cce599953418cf5f9b3cefae1d6a54c2
lol221beta <clenfer693@gmail.com>
2024-12-23 21:36:31 +0300

    Shifr of Cesar-2

--- ecryption.py
+++ ecryption.py
@@ -1,17 +1,44 @@
+# Функция шифрования обобщенным шифром Цезаря
 def encrypt(k, m):
     return ''.join(map(chr, [(ord(c) + k) % 65536 for c in m]))
 
+# Функция дешифрования обобщенным шифром Цезаря
 def decrypt(k, m):
     return ''.join(map(chr, [(ord(c) - k) % 65536 for c in m]))
 
-# Запрос ключа и текста от пользователя
-key = int(input("Введите ключ для шифрования (целое число): "))
-text = input("Введите текст для шифрования: ")
+# Функция взлома шифра Цезаря с использованием частотного анализа
+def break_cipher(text):
+    from collections import Counter
 
-# Шифрование текста
-encrypted_text = encrypt(key, text)
-print("Зашифрованный текст:", encrypted_text)
+    # Находим самый часто встречающийся символ в шифротексте
+    counter = Counter(text)
+    most_common_char, _ = counter.most_common(1)[0]
 
-# Дешифрование текста
-decrypted_text = decrypt(key, encrypted_text)
-print("Расшифрованный текст:", decrypted_text)
+    # Код символа самого частого символа в шифротексте
+    cipher_most_common_code = ord(most_common_char)
+
+    # Код символа пробела
+    space_code = ord(' ')
+
+    # Вычисляем предполагаемый ключ
+    key = (cipher_most_common_code - space_code) % 65536
+
+    # Дешифруем текст с найденным ключом
+    decrypted_text = decrypt(key, text)
+
+    return decrypted_text, key
+
+# Основная часть программы
+if __name__ == "__main__":
+    # Запрашиваем у пользователя ключ и текст для шифрования
+    key = int(input("Введите ключ для шифрования (целое число): "))
+    plain_text = input("Введите текст для шифрования: ")
+
+    # Шифруем текст
+    encrypted_text = encrypt(key, plain_text)
+    print("\nЗашифрованный текст:\n", encrypted_text)
+
+    # Взламываем шифр без знания ключа
+    recovered_text, recovered_key = break_cipher(encrypted_text)
+    print("\nВосстановленный текст без знания ключа:\n", recovered_text)
+    print("\nНайденный ключ:", recovered_key)
